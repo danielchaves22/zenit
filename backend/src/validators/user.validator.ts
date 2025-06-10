271e0f263b7b427270950ae86eb61a215deffe4e
danielchaves22 <danielchaves2@gmail.com>
2025-06-10 18:06:09 -0300

    fix user permissions flags and editing selection

--- backend/src/validators/user.validator.ts
+++ backend/src/validators/user.validator.ts
@@ -5,7 +5,9 @@ export const createUserSchema = z.object({
   password: z.string().nonempty({ message: 'Password é obrigatório.' }),
   name: z.string().min(1, { message: 'Nome é obrigatório.' }),
   companyId: z.number({ invalid_type_error: 'companyId deve ser um número.' }),
-  newRole: z.enum(['ADMIN', 'SUPERUSER', 'USER']).optional()
+  newRole: z.enum(['ADMIN', 'SUPERUSER', 'USER']).optional(),
+  manageFinancialAccounts: z.boolean().optional(),
+  manageFinancialCategories: z.boolean().optional()
 });
 
 export const updateUserSchema = z
@@ -14,7 +16,9 @@ export const updateUserSchema = z
     password: z.string().nonempty({ message: 'Password não pode ser vazio.' }).optional(),
     name: z.string().min(1, { message: 'Nome é obrigatório.' }).optional(),
     companyId: z.number({ invalid_type_error: 'companyId deve ser um número.' }).optional(),
-    newRole: z.enum(['ADMIN', 'SUPERUSER', 'USER']).optional()
+    newRole: z.enum(['ADMIN', 'SUPERUSER', 'USER']).optional(),
+    manageFinancialAccounts: z.boolean().optional(),
+    manageFinancialCategories: z.boolean().optional()
   })
   .refine((data) => Object.keys(data).length > 0, {
     message: 'Ao menos um campo deve ser fornecido para atualização.'
